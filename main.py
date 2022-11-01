94da76c5bf24080773704859d349ca0e123bf854
Marco Resendiz <113551818+maresendiz2@users.noreply.github.com>
2022-11-01 20:20:42 +0000

    pushed it

--- main.py
+++ main.py
@@ -7,23 +7,43 @@ from addfruit import AddFruit
 from dividefruit import DivideFruit
 from subtractfruit import SubtractFruit
 from displayfruit import DisplayFruit
-apples = int(input("how many apples?"))
-oranges = int(input("how many oranges?"))
+
+# apples = int(input("how many apples?"))
+# oranges = int(input("how many oranges?"))
 
 # add fruit
 #whenever you return items, you must put the
 #returned value inside of a variable
-fruitAdded = AddFruit(apples,oranges)
-print(fruitAdded)
+# fruitAdded = AddFruit(apples,oranges)
+# print(fruitAdded)
 # subtract fruit
 
-fruitSubtracted = SubtractFruit(apples,oranges)
-print(fruitSubtracted)
+# fruitSubtracted = SubtractFruit(apples,oranges)
+# print(fruitSubtracted)
 
 # divide fruit
 
-fruitDivided = DivideFruit(apples,oranges)
-print(int(fruitDivided))
+# fruitDivided = DivideFruit(apples,oranges)
+# print(int(fruitDivided))
 # display the added fruit, subtracted fruit, and divided fruit
-fruitDisplayed = DisplayFruit(fruitAdded,fruitSubtracted,fruitDivided)
-print(fruitDisplayed)
+# fruitDisplayed = DisplayFruit(fruitAdded,fruitSubtracted,fruitDivided)
+# print(fruitDisplayed)
+word = str(input("give a word "))
+def reverse_word(word):
+  capitalizied = word.upper()
+  splitted = capitalizied.split()
+  reverse_splitted = splitted.reverse()
+  word_reversed = reverse_splitted.join()
+  return word_reversed
+
+def print_function(reverse_word):
+  print(reverse_word)
+
+# word = input("give a word ")
+# def reverse_word(word):
+  
+#   splitted = word.split()
+#   reverse_splitted = splitted.reverse()
+#   print(reverse_splitted)
+
+# reverse_word(word)
